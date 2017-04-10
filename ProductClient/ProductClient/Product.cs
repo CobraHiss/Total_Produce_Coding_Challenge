6f870c9157f4ad640a43703271f04ceb7a9b5e24
George <microncore@yahoo.co.uk>
2017-04-10 17:20:31 +0100

    Client

---
+++ ProductClient/ProductClient/Product.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ProductClient
+{
+    public class Product : INotifyPropertyChanged
+    {
+        public Product(int productIdin, string productNameIn, string productTypeIn, string productDescriptionIn, double productPriceIn)
+        {
+            this.ProductId = productIdin;
+            this.ProductName = productNameIn;
+            this.ProductType = productTypeIn;
+            this.ProductDescription = productDescriptionIn;
+            this.ProductPrice = productPriceIn;
+        }
+        
+        private int productId;
+        public int ProductId
+        {
+            get { return productId; }
+            set
+            {
+                if (productId == value)
+                    return;
+                productId = value;
+                OnPropertyChanged("ProductId");
+            }
+        }
+
+        private string productName;
+        public string ProductName
+        {
+            get { return productName; }
+            set
+            {
+                if (productName == value)
+                    return;
+                productName = value;
+                OnPropertyChanged("ProductName");
+            }
+        }
+
+        private string productType;
+        public string ProductType
+        {
+            get { return productType; }
+            set
+            {
+                if (productType == value)
+                    return;
+                productType = value;
+                OnPropertyChanged("ProductType");
+            }
+        }
+
+        private string productDescription;
+        public string ProductDescription
+        {
+            get { return productDescription; }
+            set
+            {
+                if (productDescription == value)
+                    return;
+                productDescription = value;
+                OnPropertyChanged("ProductDescription");
+            }
+        }
+
+        private double productPrice;
+        public double ProductPrice
+        {
+            get { return productPrice; }
+            set
+            {
+                if (productPrice == value)
+                    return;
+                productPrice = value;
+                OnPropertyChanged("ProductPrice");
+            }
+        }
+
+        private void OnPropertyChanged(string v)
+        {}
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {}
+
+            remove
+            {}
+        }
+    }
+}
---
+++ ProductClient/ProductClient/ViewModels/ProductsListsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+using ProductClient.ProductServiceRef;
+using System.Collections.ObjectModel;
+
+namespace ProductClient.ViewModels
+{
+    [POCOViewModel]
+    public class ProductsListsViewModel
+    {
+        public virtual ObservableCollection<Product> ProductsList { get; set; }
+
+        protected ProductsListsViewModel()
+        {
+            IProductService p = new ProductServiceClient();
+
+            ProductsList = new ObservableCollection<Product>();
+
+            foreach (var productItem in p.GetAllProducts())
+            {
+                ProductsList.Add(new ProductClient.Product(productItem.ProductId, productItem.ProductName, productItem.ProductType, productItem.ProductDescription, productItem.ProductPrice));
+                System.Diagnostics.Debug.WriteLine(productItem.ProductName);
+            }
+        }
+
+        public static ProductsListsViewModel Create()
+        {
+            return ViewModelSource.Create(() => new ProductsListsViewModel());
+        }
+    }
+}
